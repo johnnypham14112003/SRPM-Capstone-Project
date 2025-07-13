f0aec7db6ab710024c654194c62e8914f6d720f8
Johnny Pham <johnnypham14112003@gmail.com>
2025-07-14 04:25:54 +0700

    fix: Refactor Controllers and Models class Code

--- SRPM/SRPM_Services/BusinessModels/RequestModels/RQ_AppraisalCouncil.cs
+++ SRPM/SRPM_Services/BusinessModels/RequestModels/RQ_AppraisalCouncil.cs
@@ -8,5 +8,4 @@ public class RQ_AppraisalCouncil
     public DateTime? CreatedAt { get; set; }
     public DateTime? UpdatedAt { get; set; }
     public string Status { get; set; } = "created";
-    public byte IncludeNo { get; set; } = 0; //1:Evaluations | 2:EvaluationStages | 3:Members | 12, 13, 23...
 }
