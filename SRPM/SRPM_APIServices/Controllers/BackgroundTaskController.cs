63e08140fea105ef50e32ec11d4ba47538593f4a
Johnny Pham <johnnypham14112003@gmail.com>
2025-08-06 18:40:30 +0700

    feat: Add API manage background ServerTask

---
+++ SRPM/SRPM_APIServices/Controllers/BackgroundTaskController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SRPM_Services.Extensions.BackgroundService;
+
+namespace SRPM_APIServices.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class BackgroundTaskController : Controller
+{
+    private readonly ITaskQueueHandler _taskQueueHandler;
+
+    public BackgroundTaskController(ITaskQueueHandler taskQueueHandler)
+    {
+        _taskQueueHandler = taskQueueHandler;
+    }
+
+    [HttpGet("{taskId}")]
+    public IActionResult GetStatus([FromRoute] string taskId)
+    {
+        var meta = _taskQueueHandler.GetTaskMetadata(taskId);
+        if (meta == null) return NotFound();
+        return Ok(new
+        {
+            TaskId = meta.TaskId,
+            Status = meta.Status.ToString()
+        });
+    }
+
+    [HttpDelete("{taskId}")]
+    public IActionResult Cancel([FromRoute] string taskId)
+    {
+        var success = _taskQueueHandler.CancelTask(taskId);
+        if (!success) return NotFound("Cannot Cancel Or It Has Been Completed!");
+        return Ok("Cancelled Task");
+    }
+}
