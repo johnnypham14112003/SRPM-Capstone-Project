8c60f1ff8ec9fddf152d7793c12087bee35b793b
Johnny Pham <johnnypham14112003@gmail.com>
2025-08-17 23:41:54 +0700

    fix: email template, syntax model

--- SRPM/SRPM_Services/BusinessModels/Others/RQ_NotificationEmail.cs
+++ SRPM/SRPM_Services/BusinessModels/Others/RQ_NotificationEmail.cs
@@ -5,10 +5,10 @@ namespace SRPM_Services.BusinessModels.Others;
 public class RQ_NotificationEmail
 {
     [Required] public string Subject { get; set; } = null!;
-    [Required] public string Body { get; set; } = null!;
+    public string? Body { get; set; }
     [Required] public string ReceiverEmailAddress { get; set; } = null!;
-    public IEnumerable<string>? ListAddressToCC { get; set; }
-    public IEnumerable<string>? ListAddressToBCC { get; set; }
+    public IEnumerable<string>? ListAddressToCc { get; set; }
+    public IEnumerable<string>? ListAddressToBcc { get; set; }
 
     public string Title { get; set; } = null!;
     public string Content { get; set; } = null!;//email description
@@ -16,5 +16,5 @@ public class RQ_NotificationEmail
     public string? RefTitle { get; set; }//name object
     public string? RefContent { get; set; }//content object
     public string? RefButton { get; set; }//name object button
-    public string? RefURL { get; set; }//link to object detail
+    public string? RefUrl { get; set; }//link to object detail
 }
