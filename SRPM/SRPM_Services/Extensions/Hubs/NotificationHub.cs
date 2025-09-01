60f3fbdbfb7152a2e3ffb341688ce3063165cc15
ttlihao <thienphan20122003@gmail.com>
2025-09-01 14:37:43 +0700

    Add real-time notification via SignalR
    Introduced NotificationHub using SignalR for real-time notifications. Updated NotificationService to send notifications instantly to users or globally through SignalR, and exposed new methods in INotificationService for real-time delivery.

---
+++ SRPM/SRPM_Services/Extensions/Hubs/NotificationHub.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SRPM_Services.Extensions.Hubs
+{
+    [Authorize]
+    public class NotificationHub : Hub
+    {
+        public async Task JoinGroup(string groupName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+        }
+
+        public async Task LeaveGroup(string groupName)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            var userId = Context.UserIdentifier;
+            if (!string.IsNullOrEmpty(userId))
+            {
+                await Groups.AddToGroupAsync(Context.ConnectionId, $"user_{userId}");
+            }
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            await base.OnDisconnectedAsync(exception);
+        }
+    }
+}
